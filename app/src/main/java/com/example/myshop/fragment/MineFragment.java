adcf37f029d029caba8f30bea26b2bde08886f7d
zouqile <793314021@qq.com>
2016-03-08 23:07:12 +0800

    登陆界面布局

--- app/src/main/java/com/example/myshop/fragment/MineFragment.java
+++ app/src/main/java/com/example/myshop/fragment/MineFragment.java
@@ -1,6 +1,7 @@
 package com.example.myshop.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,11 +10,12 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.myshop.R;
+import com.example.myshop.activity.LoginActivity;
 
 /**
  * 我的
  */
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener {
 
 
     public MineFragment() {
@@ -25,19 +27,25 @@ public class MineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        //((View) view.findViewById(R.id.mine_item_view_shop_cart)).setOnClickListener(this);//购物车(拿不到)
+        ((View) view.findViewById(R.id.mime_head_img)).setOnClickListener(this);//头像
+        ((View) view.findViewById(R.id.mine_tv_loginorreg)).setOnClickListener(this);//注册|登录
+        return view;
     }
 
-    
 
-    //在界面直接注册了事件了
-    public void mineItemViewClick(View view) {
+    @Override
+    public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.MineItemView_shop_cart:
+            case R.id.mine_item_view_shop_cart:
                 Toast.makeText(getActivity(), "购物车", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.mime_head_img:
+            case R.id.mine_tv_loginorreg:
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                break;
         }
-
     }
-
 }
